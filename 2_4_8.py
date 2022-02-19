c3dad2ec8a6e9489ed201a3566c0a52b8abad1e2
gen1usup <715702@gmail.com>
2022-02-19 18:22:45 +0300

    удалены лишние комментарии
    удалены закомменченные пробные строчки кода

--- 2_4_8.py
+++ 2_4_8.py
@@ -32,27 +32,6 @@ try:
     answer = browser.switch_to.alert.text.split(": ")[1]
     print(answer)
     browser.switch_to.alert.accept()
-    # browser.find_element_by_xpath('//button[@class="btn btn-primary"]').click()
-    # browser.find_element_by_xpath('//input[@name="file"]').send_keys('E:\\class_sb1\\2_1_step_5.txt')
-    # browser.get("https://stepik.org/lesson/181384/step/8?unit=156009")
-    # browser.find_element_by_css_selector('.quiz-component.ember-view').send_keys(answer)
-
-
-
-    #
-    # x_element = browser.find_element_by_id("input_value")
-    # x = int(x_element.text)
-    # y = calc(x)
-    # browser.find_element_by_id("answer").send_keys(y)
-    # browser.find_element_by_id('robotCheckbox').click()
-    # radiobutton = browser.find_element_by_xpath('//label[@for="robotsRule"]')
-    # browser.execute_script("return arguments[0].scrollIntoView(true);", radiobutton)
-    # radiobutton.click()
-    # button = browser.find_element_by_class_name("container .btn")
-    # browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-    # button.click()
-    # print(os.path.abspath(__file__))
-    # print(os.path.abspath(os.path.dirname(__file__)))
 
 except:
     time.sleep(10)
